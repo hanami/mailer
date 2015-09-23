7d1144f6f7f910e2f5eb2c1c887187b1bc036ec6
Luca Guidi <me@lucaguidi.com>
2015-09-23 18:13:58 +0200

    Added pending test for custom delivery method

--- test/delivery_test.rb
+++ test/delivery_test.rb
@@ -120,47 +120,43 @@ describe Lotus::Mailer do
       end
     end
 
-    # describe 'custom delivery' do
-    #   before do
-    #     @options = options = { deliveries: [] }
+    describe 'custom delivery' do
+      before do
+        @options = options = { deliveries: [] }
 
-    #     Lotus::Mailer.reset!
-    #     Lotus::Mailer.configure do
-    #       delivery_method MandrillDeliveryMethod, options
-    #     end.load!
+        # Lotus::Mailer.reset!
+        # Lotus::Mailer.configure do
+        #   delivery_method MandrillDeliveryMethod, options
+        # end.load!
 
-    #     WelcomeMailer.deliver
+        WelcomeMailer.deliver
 
-    #     @mail = options.fetch(:deliveries).first
-    #   end
+        @mail = options.fetch(:deliveries).first
+      end
 
-    #   after do
-    #     Lotus::Mailer.reset!
-    #   end
+      it 'delivers the mail'
+      # it 'delivers the mail' do
+      #   @options.fetch(:deliveries).length.must_equal 1
+      # end
 
-    #   it 'delivers the mail' do
-    #     @options.fetch(:deliveries).length.must_equal 1
-    #   end
+      # it 'sends the correct information' do
+      #   @mail.from.must_equal ['noreply@sender.com']
+      #   @mail.to.must_equal ['noreply@recipient.com']
+      #   @mail.subject.must_equal "Welcome"
+      # end
 
-    #   it 'sends the correct information' do
-    #     @mail.from.must_equal ['noreply@sender.com']
-    #     @mail.to.must_equal ['noreply@recipient.com']
-    #     @mail.subject.must_equal "Welcome"
-    #   end
+      # it 'has the correct templates' do
+      #   @mail.html_part.to_s.must_include %(template)
+      #   @mail.text_part.to_s.must_include %(template)
+      # end
 
-    #   it 'has the correct templates' do
-    #     @mail.html_part.to_s.must_include %(template)
-    #     @mail.text_part.to_s.must_include %(template)
-    #     @mail.attachments['welcome_mailer.csv'].wont_be_nil
-    #   end
+      # it 'interprets the prepare statement' do
+      #   @mail.attachments['invoice.pdf'].wont_be_nil
+      # end
 
-    #   it 'interprets the prepare statement' do
-    #     @mail.attachments['invoice.pdf'].wont_be_nil
-    #   end
-
-    #   it 'adds the attachment to the mail object' do
-    #     @mail.attachments['render_mailer.html.erb'].wont_be_nil
-    #   end
-    # end
+      # it 'adds the attachment to the mail object' do
+      #   @mail.attachments['render_mailer.html.erb'].wont_be_nil
+      # end
+    end
   end
 end
