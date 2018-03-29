9cc345c91bf054efac351a414e338cb9dcb48127
Luca Guidi <me@lucaguidi.com>
2018-03-29 16:13:31 +0200

    Prepare for v1.2.0.rc1

--- spec/unit/hanami/mailer/version_spec.rb
+++ spec/unit/hanami/mailer/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Mailer::VERSION" do
   it "returns current version" do
-    expect(Hanami::Mailer::VERSION).to eq("1.2.0.beta2")
+    expect(Hanami::Mailer::VERSION).to eq("1.2.0.rc1")
   end
 end
